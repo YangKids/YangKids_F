7054141aa30e88eaa6be185571955077618fc077
YUJUNG <heoyujung0118@gamil.com>
2023-05-12 17:33:22 +0900

    아직 라우팅 안되니까 App.js에 회원가입 페이지 만들어 보는 중!

--- src/App.js
+++ src/App.js
@@ -1,44 +1,56 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/layout/header/Header';
-import FreeBoard from './components/FreeBoard';
-import Footer from './components/layout/Footer';
-import HotArticle from './components/HotArticle';
-import Quot from './components/carousel';
-import SearchBar from './components/SearchBar';
-import YangchelinGuide from './components/YangchelinGuide';
+// import React from 'react';
+// import './App.css';
+// import Navbar from './components/layout/header/Header';
+// import FreeBoard from './components/FreeBoard';
+// import Footer from './components/layout/Footer';
+// import HotArticle from './components/HotArticle';
+// import Quot from './components/carousel';
+// import SearchBar from './components/SearchBar';
+// import YangchelinGuide from './components/YangchelinGuide';
 
+// const App = () => {
+//   return (
+//     <div>
+//       <div className='Body'>
+//         <div style={{ height: '100px', display: 'flex', justifyContent: "center", alignContent: "center" }}>
+//           <Navbar />
+//         </div>
+
+//         <div className='Quotation'><Quot /></div>
+
+//         <div className='SearchBox'><SearchBar /></div>
+
+//         <div className='CardBox'>
+//           <FreeBoard />
+//           <div style={{ minWidth: '40px', height: '1410px'}}></div>
+//           <HotArticle />
+//         </div>
+
+//         <div className='CardBox'>
+//         <HotArticle />
+//         <div style={{ minWidth: '40px', height: '1410px'}}></div>
+//           <YangchelinGuide/>
+//         </div>
+//         <Footer />
+//       </div>
+
+//     </div>
+
+//   )
+// };
+// export default App;
+import React from "react";
+import "./App.css";
+import Navbar from "./components/layout/header/Header";
 
 const App = () => {
   return (
     <div>
-      <div className='Body'>
-        <div style={{ height: '100px', display: 'flex', justifyContent: "center", alignContent: "center" }}>
-          <Navbar />
-        </div>
-
-        <div className='Quotation'><Quot /></div>
-
-        <div className='SearchBox'><SearchBar /></div>
-
-        <div className='CardBox'>
-          <FreeBoard />
-          <div style={{ minWidth: '40px', height: '1410px'}}></div>
-          <HotArticle />
-        </div>
-
-        <div className='CardBox'>
-        <HotArticle />
-        <div style={{ minWidth: '40px', height: '1410px'}}></div>
-          <YangchelinGuide/>
-        </div>
-        <Footer />
+      <div className='Body'> 
+        <Navbar />
+        <h1>회원가입</h1>
       </div>
-
-
     </div>
-
-
-  )
+  );
 };
 export default App;
---
+++ src/components/User/UserSignUp.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import './UserSignUp.css';
+// import Navbar from '../layout/header/Header';
+
+// const UserSignUp = () => {
+//     return (
+//         <div>
+//             <Navbar />
+//         </div>
+//     )
+// }
+// export default UserSignUp;
