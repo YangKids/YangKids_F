b357df9fd58f4c83ef98fe2a3bafdf538831d097
YUJUNG <heoyujung0118@gmail.com>
2023-05-15 00:25:12 +0900

    회원가입 페이지 건드려보는 중 (아직 완전 미완성)

--- src/App.js
+++ src/App.js
@@ -39,18 +39,56 @@
 //   )
 // };
 // export default App;
+
 import React from "react";
 import "./App.css";
 import Navbar from "./components/layout/header/Header";
+import { useForm } from "react-hook-form";
 
-const App = () => {
+function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <div>
-      <div className='Body'> 
-        <Navbar />
+    <div className="App">
+      <Navbar />
+      <div className="Body">
         <h1>회원가입</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            name="email"
+            placeholder="이메일"
+            {...register("email", { required: "이메일을 입력해주세요" })}
+          />
+          {errors.email && <p>{errors.email.message}</p>}
+          <input
+            type="password"
+            name="password"
+            placeholder="비밀번호"
+            {...register("password", {
+              required: "비밀번호를 입력해주세요",
+              minLength: {
+                value: 6,
+                message: "최소 6자 이상의 비밀번호를 입력해주세요",
+              },
+            })}
+          />
+          {errors.password && <p>{errors.password.message}</p>}
+          <input
+            type="password"
+            name="passwordConfirm"
+            placeholder="비밀번호 확인"
+            {...register("passwordConfirm")}
+          />
+          <input type="submit" />
+        </form>
       </div>
     </div>
   );
-};
+}
+
 export default App;
